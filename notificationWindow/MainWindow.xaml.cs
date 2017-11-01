f4c729888b357a3073cbe33631bfb5f3d41ef380
Harvey <Michael.Harvey@acer.com>
2017-11-01 14:24:58 -0700

    first

---
+++ notificationWindow/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace notificationWindow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            double h = this.ActualHeight;
+
+            double hh = System.Windows.SystemParameters.PrimaryScreenHeight - 750;
+            double ww = System.Windows.SystemParameters.PrimaryScreenWidth - 920;
+
+            this.Top = hh;
+            this.Left = ww;
+            
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
